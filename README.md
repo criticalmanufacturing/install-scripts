3e98792e3f8a0530d3fdc50f411b00783640fd05
Adélio Fernandes <AdelioFernandes@criticalmanufacturing.com>
2021-02-11 19:32:10 +0000

    minor changes

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Critical Manufacturing Installation Scripts
 ## Prepare single server Ubuntu 20.04 environment
 
 ```
-$ curl -L https://raw.githubusercontent.com/criticalmanufacturing/install-scripts/main/ubuntu/20.04/install.bash | bash
+$ curl -fsSL https://raw.githubusercontent.com/criticalmanufacturing/install-scripts/main/ubuntu/20.04/install.bash | bash
 ```
 
 In case you want to pass your own Portainer admin password
 
 ```
-$ curl -L https://raw.githubusercontent.com/criticalmanufacturing/install-scripts/main/ubuntu/20.04/install.bash | bash -s -- -password <your password>
+$ curl -fsSL https://raw.githubusercontent.com/criticalmanufacturing/install-scripts/main/ubuntu/20.04/install.bash | bash -s -- -password <your password>
 ```
--- ubuntu/20.04/install.bash
+++ ubuntu/20.04/install.bash
@@ -23,7 +23,7 @@ done
 if [[ $portainerPassword = "" ]]
 then
   #password is empty -> let's generate a new one
-  response=`curl 'https://passwordsgenerator.net/calc.php?Length=16&Symbols=1&Lowercase=1&Uppercase=1&Numbers=1&Nosimilar=1&Last=1317'`
+  response=`curl -fsSL 'https://passwordsgenerator.net/calc.php?Length=16&Symbols=1&Lowercase=1&Uppercase=1&Numbers=1&Nosimilar=1&Last=1317'`
   portainerPassword=${response:0:16}
 fi
 
@@ -31,33 +31,24 @@ echo "PortainerPassword: $portainerPassword"
 echo "SecurityToken: $securityToken"
 
 #disable swap
-swapoff -a;
-sed -i 's/\/swap.img/#\/swap.img/g' /etc/fstab;
+swapoff -a
+sed -i 's/\/swap.img/#\/swap.img/g' /etc/fstab
 
 #install docker
 curl -fsSL "$REPOSITORY/ubuntu/installDocker.bash" | bash;
 
 #Deploy portainer stack
-wget -q "$REPOSITORY/utils/portainer-agent-stack.yml";
-hashedPassword=$(htpasswd -nbB admin $portainerPassword | cut -d ":" -f 2 | sed 's+\$+$$+g');
-pattern='s+ADMIN_PASSWORD+'$hashedPassword'+g';
-sed -i "$pattern" portainer-agent-stack.yml;
-docker stack deploy -c portainer-agent-stack.yml portainer;
-portainer-agent-stack.yml;
+wget -q "$REPOSITORY/utils/portainer-agent-stack.yml"
+hashedPassword=$(htpasswd -nbB admin $portainerPassword | cut -d ":" -f 2 | sed 's+\$+$$+g')
+pattern='s+ADMIN_PASSWORD+'$hashedPassword'+g'
+sed -i "$pattern" portainer-agent-stack.yml
+docker stack deploy -c portainer-agent-stack.yml portainer
+rm -f portainer-agent-stack.yml
 
 # Install PowerShell
 curl -fsSL "$REPOSITORY/ubuntu/20.04/installPowershell.bash" | bash;
 
 # Start PowerShell
-wget -q "$REPOSITORY/utils/createPortainerStack.ps1";
-pwsh ./createPortainerStack.ps1 -StackName portainer -PortainerUser admin -PortainerPassword "$portainerPassword" -StackFileName ./portainer-agent-stack.yml;
-rm -f createPortainerStack.ps1;
-
-#Output
-echo "#####################################";
-echo " Portainer is running:";
-echo "";
-echo " Url:       http://localhost:9000";
-echo " User:      admin";
-echo " Password:  $portainerPassword";
-echo "#####################################";
+wget -q "$REPOSITORY/utils/createPortainerStack.ps1"
+pwsh ./createPortainerStack.ps1 -StackName portainer -PortainerUser admin -PortainerPassword "$portainerPassword" -StackFileName ./portainer-agent-stack.yml
+rm -f createPortainerStack.ps1
