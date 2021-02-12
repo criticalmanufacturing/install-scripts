1e25a461b572e6481a27ee4beef937d2625387c4
Adélio Fernandes <AdelioFernandes@criticalmanufacturing.com>
2021-02-12 00:40:58 +0000

    minor changes

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,3 +1,3 @@
 $RepositoryUrl = "https://raw.githubusercontent.com/criticalmanufacturing/install-scripts/main"
 Invoke-WebRequest -Uri "$RepositoryUrl/utils/deployPortainer.ps1" -OutFile deployPortainer.ps1
-./deployPortainer.ps1 -RepositoryUrl $REPOSITORY
+./deployPortainer.ps1 -RepositoryUrl $RepositoryUrl
