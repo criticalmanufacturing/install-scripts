f956b5eaf6eeb6d65b49c68092cd6e590e3d433a
Adélio Fernandes <AdelioFernandes@criticalmanufacturing.com>
2021-02-12 01:08:53 +0000

    minor changes

--- utils/deployPortainer.ps1
+++ utils/deployPortainer.ps1
@@ -11,21 +11,24 @@ if ($PortainerPassword -eq "")
 }
 Write-Host $RepositoryUrl
 #Download files
+Write-Host "Downloading file $RepositoryUrl/utils/portainer-agent-stack.yml"
 Invoke-WebRequest -Uri "$RepositoryUrl/utils/portainer-agent-stack.yml" -OutFile portainer-agent-stack.yml
+Write-Host "Downloading file $RepositoryUrl/utils/createStackInPortainer.ps1"
 Invoke-WebRequest -Uri "$RepositoryUrl/utils/createStackInPortainer.ps1" -OutFile createStackInPortainer.ps1
 
 #Hash portainer password
 docker pull httpd:2.4-alpine
 $hashedPassword = (docker run --rm httpd:2.4-alpine htpasswd -nbB admin $PortainerPassword)
-$hashedPassword = ($hashedPassword -replace '\$','$$$').Split(":")[1]
-(Get-Content -Path portainer-agent-stack.yml -Raw) -replace "ADMIN_PASSWORD",$hashedPassword | Set-Content -Path portainer-agent-stack.yml
+$hashedPassword = $hashedPassword.Split(":")[1]
+Write-Host "Hashed password: $hashedPassword"
+((Get-Content -Path portainer-agent-stack.yml -Raw) -replace "ADMIN_PASSWORD", $hashedPassword) -replace  '\$','$$$' | Set-Content -Path portainer-agent-stack.yml
 docker stack deploy -c portainer-agent-stack.yml portainer
 #Deploy the portainer stack within portainer (avoid it to be marked as external)
 ./createStackInPortainer.ps1 -StackName portainer -PortainerUser admin -PortainerPassword "$portainerPassword" -StackFileName ./portainer-agent-stack.yml
 
 #Clean up 
-Remove-Item -Name portainer-agent-stack.yml
-Remove-Item -Name createStackInPortainer.ps1
+Remove-Item ./portainer-agent-stack.yml
+Remove-Item ./createStackInPortainer.ps1
 
 Write-Host "Portainer is Installed"
 Write-Host "Url:       http://localhost:9000"
--- windows/install.ps1
+++ windows/install.ps1
@@ -1,3 +1,5 @@
 $RepositoryUrl = "https://raw.githubusercontent.com/criticalmanufacturing/install-scripts/main"
 Invoke-WebRequest -Uri "$RepositoryUrl/utils/deployPortainer.ps1" -OutFile deployPortainer.ps1
 ./deployPortainer.ps1 -RepositoryUrl "$RepositoryUrl"
+#Clean up 
+Remove-Item ./deployPortainer.ps1
