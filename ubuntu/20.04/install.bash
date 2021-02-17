5a91781465d518fdd74e598dfdfba0c84434d12c
adeliofernandes <adelio@gmail.com>
2021-02-17 19:28:02 +0000

    fix

--- ubuntu/20.04/install.bash
+++ ubuntu/20.04/install.bash
@@ -35,5 +35,5 @@ curl -fsSL "$REPOSITORY/ubuntu/20.04/installPowershell.bash" | bash;
 
 #Deploy portainer
 wget -q "$REPOSITORY/utils/deployPortainer.ps1"
-pwsh -File ./deployPortainer.ps1 -RepositoryUrl $REPOSITORY >&1
+pwsh -File ./deployPortainer.ps1 -RepositoryUrl $REPOSITORY
 rm -f deployPortainer.ps1
