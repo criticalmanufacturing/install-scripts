8ed0e31b83c1071d4bc9d112d2d4b0ca4e1001b4
Francisco Maciel <FranciscoMaciel@criticalmanufacturing.com>
2021-06-21 19:01:35 +0100

    Fix parameters

--- windows/portal/initializeInfrastructureFromTemplate.ps1
+++ windows/portal/initializeInfrastructureFromTemplate.ps1
@@ -14,6 +14,6 @@ param (
 
 $global:ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/criticalmanufacturing/install-scripts/linux-infrasctucture/utils/portal/runInitializeInfrastructureFromTemplate.ps1" -OutFile "./runInitializeInfrastructureFromTemplate.ps1"
-.\runInitializeInfrastructureFromTemplate.ps1 -agent "$agent" -license "$license" -site "$site" -infrastructure "$infrastructure" -infrastructureTemplate "$infrastructureTemplate" -environmentType "$environmentType" -agentVersion "$agentVersion" -parameters="$parameters" -internetNetworkName "$internetNetworkName" -portalToken "$portalToken"
+.\runInitializeInfrastructureFromTemplate.ps1 -agent "$agent" -license "$license" -site "$site" -infrastructure "$infrastructure" -infrastructureTemplate "$infrastructureTemplate" -environmentType "$environmentType" -agentVersion "$agentVersion" -parameters "$parameters" -internetNetworkName "$internetNetworkName" -portalToken "$portalToken"
 Remove-Item -Path ./runInitializeInfrastructureFromTemplate.ps1
 $global:ProgressPreference = 'Continue'
