32e8a03e30a0a35b74c944b99ae5b1341769e754
Marco <marcosilva@criticalmanufacturing.com>
2022-09-19 11:35:53 +0100

    feat: support debian systems
    Adds support for Debian systems, while maintaining Ubuntu support.
    This was tested in Debian Bullseye.

--- ubuntu/installDocker.bash
+++ ubuntu/installDocker.bash
@@ -1,14 +1,15 @@
 #update system
 apt update -y -qq
 apt upgrade -y -qq
+. /etc/os-release
 #remove old docker version
 apt-get remove docker docker-engine docker.io containerd runc -qq -y
 #install utils
 apt-get install wget apt-transport-https ca-certificates curl gnupg-agent software-properties-common apache2-utils -qq -y
 #install DOCKER CE
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt update -y -qq 
+curl -fsSL https://download.docker.com/linux/$ID/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$ID $VERSION_CODENAME stable"
+apt update -y -qq
 apt-get install docker-ce docker-ce-cli containerd.io -y -qq
 apt update -y -qq
 apt upgrade -y -qq
@@ -25,19 +26,17 @@ echo "{
     }
 }" > /etc/docker/daemon.json
 
-if [[ $(grep WSL /proc/version) ]]; 
+if [[ $(grep WSL /proc/version) ]];
 then
-	echo "Bash is running on WSL"
-	# Start Docker daemon automatically when logging in if not running.
-	RUNNING=`ps aux | grep dockerd | grep -v grep`
-	if [ -z "$RUNNING" ]; 
-	then
-		echo "Staring dockerd"
-		sudo dockerd > /dev/null 2>&1 & disown
-	fi
+        echo "Bash is running on WSL"
+        # Start Docker daemon automatically when logging in if not running.
+        RUNNING=`ps aux | grep dockerd | grep -v grep`
+        if [ -z "$RUNNING" ];
+        then
+                echo "Staring dockerd"
+                sudo dockerd > /dev/null 2>&1 & disown
+        fi
 else
-	#reload docker config
-	systemctl reload docker
+        #reload docker config
+        systemctl reload docker
 fi
-
-
