682e2e3f784b8f519d37c5a612b61cc45e6d7536
Rita Norinho <AnaNorinho@criticalmanufacturing.com>
2022-09-28 16:11:37 +0100

    Remove customer infrastructure domain

--- windows/portal/initializeInfrastructure.ps1
+++ windows/portal/initializeInfrastructure.ps1
@@ -2,7 +2,6 @@ param (
     [Parameter(Mandatory=$true)][string]$agent,
     [Obsolete("No longer needed. Value ignored.")][string]$license,
     [Parameter(Mandatory=$true)][string]$infrastructure,
-    [Parameter(Mandatory=$true)][string]$domain,
     [string]$customer,
     #optional parameters
     [string] $environmentType,
@@ -16,6 +15,6 @@ param (
 
 $global:ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/criticalmanufacturing/install-scripts/main/utils/portal/runInitializeInfrastructure.ps1" -OutFile "./runInitializeInfrastructure.ps1"
-.\runInitializeInfrastructure.ps1 -agent "$agent" -site "$site" -customer "$customer" -infrastructure "$infrastructure" -domain "$domain" -environmentType "$environmentType" -parameters "$parameters" -internetNetworkName "$internetNetworkName" -portalToken "$portalToken"
+.\runInitializeInfrastructure.ps1 -agent "$agent" -site "$site" -customer "$customer" -infrastructure "$infrastructure" -environmentType "$environmentType" -parameters "$parameters" -internetNetworkName "$internetNetworkName" -portalToken "$portalToken"
 Remove-Item -Path ./runInitializeInfrastructure.ps1
 $global:ProgressPreference = 'Continue'
