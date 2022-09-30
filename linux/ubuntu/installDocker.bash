b592f3a63918d34a3609c8d736054ef107e9dcc7
Miguel Garcia <MiguelGarcia@criticalmanufacturing.com>
2022-09-30 14:31:22 +0100

    change ubuntu install scripts

--- linux/ubuntu/installDocker.bash
+++ linux/ubuntu/installDocker.bash
@@ -3,20 +3,15 @@ set -e
 
 #update system
 apt update -y -qq
-apt upgrade -y -qq
+
 . /etc/os-release
-#remove old docker version
-apt-get remove docker docker-engine docker.io containerd runc -qq -y
+
 #install utils
 apt-get install wget apt-transport-https ca-certificates curl gnupg-agent software-properties-common apache2-utils -qq -y
+
+
 #install DOCKER CE
 curl -fsSL https://download.docker.com/linux/$ID/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$ID $VERSION_CODENAME stable"
 apt update -y -qq
-apt-get install docker-ce docker-ce-cli containerd.io -y -qq
-apt update -y -qq
-apt upgrade -y -qq
-#install Docker Compose V2
-mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y -qq
