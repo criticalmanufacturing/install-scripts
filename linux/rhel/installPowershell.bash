01287b683c1bb18b99e8a29c4c8e4762f69becd5
Miguel Garcia <MiguelGarcia@criticalmanufacturing.com>
2022-09-30 15:47:26 +0100

    Hardcode RHEL 8

--- linux/rhel/installPowershell.bash
+++ linux/rhel/installPowershell.bash
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-. /etc/os-release
-
 # Register the Microsoft RedHat repository
-curl https://packages.microsoft.com/config/rhel/$VERSION_ID/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+curl https://packages.microsoft.com/config/rhel/8/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 
 # Install PowerShell
 dnf install --assumeyes powershell
