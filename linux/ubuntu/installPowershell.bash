740461cfa2c0f4054d5e8d03b065246a7bb26284
Miguel Garcia <MiguelGarcia@criticalmanufacturing.com>
2022-10-04 16:50:47 +0100

    Update install pwsh

--- linux/ubuntu/installPowershell.bash
+++ linux/ubuntu/installPowershell.bash
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
 
-# Install powershell
 . /etc/os-release
+
+# Update the list of packages
+apt-get update
+# Install pre-requisite packages.
+apt-get install -y wget apt-transport-https software-properties-common
+# Download the Microsoft repository GPG keys
 wget -q https://packages.microsoft.com/config/$ID/$VERSION_ID/packages-microsoft-prod.deb
+# Register the Microsoft repository GPG keys
 dpkg -i packages-microsoft-prod.deb
-apt-get update -y
-add-apt-repository universe
+# Update the list of packages after we added packages.microsoft.com
+apt-get update
+# Install PowerShell
 apt-get install -y powershell
+# Remove deb file
 rm -f packages-microsoft-prod.deb
