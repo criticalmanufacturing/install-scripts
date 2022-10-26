5fb9063ae23e35bc3c2c60264f734de05641ccd0
Miguel Garcia <MiguelGarcia@criticalmanufacturing.com>
2022-10-26 10:56:36 +0100

    Fix checkEnvironment.sh

--- equipmentcenter/checkEnvironment.sh
+++ equipmentcenter/checkEnvironment.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 set -e
-if ! command -v "pwsh" > /dev/null 2>&1; then
+
+read -p 'Scripts repository [https://raw.githubusercontent.com/migafgarcia/install-scripts/main-rhel-support]: ' REPOSITORY </dev/tty
+export REPOSITORY=${REPOSITORY:-"https://raw.githubusercontent.com/migafgarcia/install-scripts/main-rhel-support"}
+
+if command -v pwsh > /dev/null 2>&1; then
     wget -q "$REPOSITORY/equipmentcenter/SystemRequirementsChecker.ps1"
     pwsh -File ./SystemRequirementsChecker.ps1
 else
