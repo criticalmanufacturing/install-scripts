b3499b8a20f06a4079c23b630011758c7f11c1c0
Rita Norinho <AnaNorinho@criticalmanufacturing.com>
2024-04-02 10:49:02 +0100

    Remove folder

--- kioskapp/public/client.js
+++
@@ -1,9 +0,0 @@
-console.log('Client-side code running');
-
-const button = document.getElementById('myButton');
-button.addEventListener('click', function(e) {
-  window.location.replace("https://portal.criticalmanufacturing.com/");
-  console.log('button was clicked');
-});
-
-
