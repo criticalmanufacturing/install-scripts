44981570181089e5af2ebe1620517aa1450151aa
Rita Norinho <AnaNorinho@criticalmanufacturing.com>
2024-04-02 15:20:03 +0100

    Add PR suggestions

--- mesinabox/kioskapp/public/client.js
+++ mesinabox/kioskapp/public/client.js
@@ -1,8 +1,9 @@
 console.log('Client-side code running');
 
 const button = document.getElementById('myButton');
+const clusterAddress = 'kioskapp.apps.172.19.73.158.nip.io';
 button.addEventListener('click', function(e) {
-  window.location.replace("https://portal.criticalmanufacturing.com/");
+  window.location.replace(`https://portaldev.criticalmanufacturing.dev/enrollWizard?clusterAddress=${encodeURIComponent(clusterAddress)}`);
   console.log('button was clicked');
 });
 
