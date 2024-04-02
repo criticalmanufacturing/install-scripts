abaed72cde03ffdcf12961fcd27b85e8f58d0efc
Rita Norinho <AnaNorinho@criticalmanufacturing.com>
2024-04-02 11:02:46 +0100

    Remove old change

--- mesinabox/stack/removeNodeJSStack.ps1
+++ mesinabox/stack/removeNodeJSStack.ps1
@@ -1,6 +1,5 @@
 oc delete -f nodetest-service.yaml
 oc delete -f nodetest-deployment.yaml
 oc delete -f nodetest-ingress.yaml
-oc delete -f nodetest-cr.yaml
 oc delete -f nodetest-crb.yaml
 oc delete -f nodetest-sa.yaml
