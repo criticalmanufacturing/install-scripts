0a8c0a98b4c78626ac09af5b2a7506bb43bec554
Rita Norinho <AnaNorinho@criticalmanufacturing.com>
2024-04-02 17:29:59 +0100

    Change dockerfile and create function to get cluster address

--- mesinabox/kioskapp/public/client.js
+++ mesinabox/kioskapp/public/client.js
@@ -1,8 +1,44 @@
-console.log('Client-side code running');
+const { exec } = require('child_process');
+
+let clusterDomain;
+
+function getClusterDomain() {
+  return new Promise((resolve, reject) => {
+    exec('oc get route -n openshift-console console -o jsonpath="{.spec.host}"', (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      if (stderr) {
+        reject(stderr);
+        return;
+      }
+      const clusterDNSName = stdout.trim(); // Extracted Cluster DNS Name
+      if (!clusterDNSName) {
+        reject('Unable to retrieve Cluster DNS Name');
+        return;
+      }
+      clusterDomain = clusterDNSName.split('.').slice(1).join('.');
+      resolve(clusterDomain);
+    });
+  });
+}
+
+// Usage
+getClusterDomain()
+  .then(domain => {
+    console.log("Cluster Domain:", domain);
+    // Use domain variable here
+    console.log("Using domain outside of the function:", clusterDomain);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
 
 const button = document.getElementById('myButton');
-const clusterAddress = 'kioskapp.apps.172.19.73.158.nip.io';
 button.addEventListener('click', function(e) {
+  getClusterDomain();
+  const clusterAddress = `kioskapp.${clusterDomain}`;
   window.location.replace(`https://portaldev.criticalmanufacturing.dev/Enroll?cluster_uri=${encodeURIComponent(clusterAddress)}`);
   console.log('button was clicked');
 });
