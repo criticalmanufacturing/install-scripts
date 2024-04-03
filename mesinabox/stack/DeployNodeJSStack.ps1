bb68493f9a89f120070927ad35b1c42c0f279046
Rita Norinho <AnaNorinho@criticalmanufacturing.com>
2024-04-03 09:54:12 +0100

    Remove secret

--- mesinabox/stack/DeployNodeJSStack.ps1
+++ mesinabox/stack/DeployNodeJSStack.ps1
@@ -3,4 +3,3 @@ oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --forc
 oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f nodetest-service.yaml
 oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f nodetest-ingress.yaml
 oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f nodetest-crb.yaml
-oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f dev.criticalmanufacturing.io-cred.yaml
--- mesinabox/stack/removeNodeJSStack.ps1
+++ mesinabox/stack/removeNodeJSStack.ps1
@@ -2,4 +2,3 @@ oc delete -f nodetest-service.yaml
 oc delete -f nodetest-deployment.yaml
 oc delete -f nodetest-ingress.yaml
 oc delete -f nodetest-crb.yaml
-oc delete -f dev.criticalmanufacturing.io-cred.yaml
