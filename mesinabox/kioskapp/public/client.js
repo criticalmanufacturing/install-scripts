f0d898a89da05e940bc6c18b9d5a1527742690f4
Rita Norinho <AnaNorinho@criticalmanufacturing.com>
2024-04-03 09:51:41 +0100

    Apply PR suggestion

--- mesinabox/kioskapp/public/client.js
+++ mesinabox/kioskapp/public/client.js
@@ -1,7 +1,7 @@
 console.log('Client-side code running');
 
 const button = document.getElementById('myButton');
-const clusterAddress = 'kioskapp.apps.172.19.73.158.nip.io';
+const clusterAddress = window.location;
 button.addEventListener('click', function(e) {
   window.location.replace(`https://portaldev.criticalmanufacturing.dev/Enroll?cluster_uri=${encodeURIComponent(clusterAddress)}`);
   console.log('button was clicked');
