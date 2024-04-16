b39b4a4b99668677ccd59ad15d57475b820b2be0
Rita Norinho <AnaNorinho@criticalmanufacturing.com>
2024-04-16 09:22:51 +0100

    Persist enrollment info in kiosk app

--- mesinabox/kioskapp/server.js
+++ mesinabox/kioskapp/server.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const { spawn } = require('child_process');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
+const dataDirectory = '/opt/app-root/src/data';
 
 // Serve static files from the public directory
 app.use(express.static('public'));
+
+// Function to check if a file exists and read its content
+function readFileIfExists(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.access(filePath, fs.constants.F_OK, (err) => {
+            if (err) {
+                // File doesn't exist
+                console.log("Error  "+err);
+                resolve(false); // Resolve with false if the file doesn't exist
+            } else {
+                // File exists, read its content
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data);
+                    }
+                });
+            }
+        });
+    });
+}
 
 // Route to handle running the PowerShell script
 app.get('/enroll', (req, res) => {
@@ -39,7 +63,34 @@ app.get('/enroll', (req, res) => {
 
 // Serve index.html for root URL
 app.get('/', (req, res) => {
+
+    const userId = process.getuid();
+    const groupId = process.getgid();
+    console.log('User ID:', userId);
+    console.log('Group ID:', groupId);
+    const filePath = path.join(dataDirectory, 'myfile.txt');
+    const content = 'Hello, world';
+   /* fs.writeFile(filePath, content, (err) => {
+        if (err) {
+        console.error('Error creating file:', err);
+        return;
+        }
+    console.log('File created successfully');
+  });*/
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+// API to fetch dynamic content
+app.get('/api/content', async (req, res) => {
+    try {
+        // Define the file path in the mounted volume
+        const filePath = path.join(dataDirectory, 'myfile.txt');
+        console.log(filePath);
+        const content = await readFileIfExists(filePath);
+        res.send(content || false); // Send false if file does not exist
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 // Start server
--- mesinabox/stack/removeNodeJSStack.ps1
+++ mesinabox/stack/removeNodeJSStack.ps1
@@ -2,3 +2,6 @@ oc delete -f nodetest-service.yaml
 oc delete -f nodetest-deployment.yaml
 oc delete -f nodetest-ingress.yaml
 oc delete -f nodetest-crb.yaml
+oc delete -f nodetest-pvc.yaml
+oc delete -f nodetest-pv.yaml
+
