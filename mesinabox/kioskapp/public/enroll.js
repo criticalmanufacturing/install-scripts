2c0e2d79ea471ed299bd9523779b52b2a55ba780
Ricardo Teixeira <RicardoTeixeira@criticalmanufacturing.com>
2024-04-17 18:45:39 +0100

    logging font change + dirty button size normalization

--- mesinabox/kioskapp/public/enroll.js
+++ mesinabox/kioskapp/public/enroll.js
@@ -1,44 +1,24 @@
 //handle Server-Sent Events
 function handleSSE() {
     const outputDiv = document.getElementById('logs-output');
-    const eventSource = new EventSource('/enrollstart');
+    const eventSource = new EventSource(`/enrollstart${window.location.search}`);
 
     // Default events
     eventSource.addEventListener('open', () => {
         console.log('Connection opened')
     });
 
-    eventSource.addEventListener('error', () => {
-        console.error("Subscription err'd")
+    // Event listener for error
+    eventSource.addEventListener('error', (event) => {
+        console.error('SSE error:', event);
         eventSource.close();
     });
 
+    // Event listener for receiving data
     eventSource.addEventListener('message', (event) => {
-        console.log(`Receive message: ${event.data}`)
         outputDiv.innerHTML += `<p>${event.data}</p>`;
         outputDiv.scrollTop = outputDiv.scrollHeight; // Scroll to bottom
     });
-    
-    eventSource.addEventListener('connected', () => {
-        console.log('Subscription successful!');
-    });
-
-    eventSource.addEventListener('current-date', (event) => {
-        console.log(`Date: ${event.data}`);
-    });
-
-
-    // Event listener for receiving data
-    // eventSource.onmessage = function(event) {
-    //     outputDiv.innerHTML += `<p>${event.data}</p>`;
-    //     outputDiv.scrollTop = outputDiv.scrollHeight; // Scroll to bottom
-    // };
-
-    // // Event listener for error
-    // eventSource.onerror = function(event) {
-    //     console.error('SSE error:', event);
-    //     eventSource.close();
-    // };
 }
 
 // Start handling SSE when the page loads
