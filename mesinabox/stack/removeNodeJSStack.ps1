51101aeee94c07fb74e2623a6e9d90c5d137df71
Rita Norinho <AnaNorinho@criticalmanufacturing.com>
2024-05-09 14:30:09 +0100

    Rename files

--- mesinabox/stack/removeNodeJSStack.ps1
+++ mesinabox/stack/removeNodeJSStack.ps1
@@ -1,6 +1,6 @@
-oc delete -f nodetest-service.yaml
-oc delete -f nodetest-deployment.yaml
-oc delete -f nodetest-ingress.yaml
-oc delete -f nodetest-crb.yaml
-oc delete -f nodetest-pvc.yaml
+oc delete -f service.yaml
+oc delete -f deployment.yaml
+oc delete -f ingress.yaml
+oc delete -f crb.yaml
+oc delete -f pvc.yaml
 
