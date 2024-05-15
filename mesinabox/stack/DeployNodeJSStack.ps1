860850e11fb2a34a12104d508047e0af3bb137ea
Rita Norinho <AnaNorinho@criticalmanufacturing.com>
2024-05-15 14:09:03 +0100

    Add PR suggestions

--- mesinabox/stack/DeployNodeJSStack.ps1
+++ mesinabox/stack/DeployNodeJSStack.ps1
@@ -1,6 +1,6 @@
 oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f namespace.yaml
 oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f deployment.yaml
 oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f service.yaml
-oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f ingress.yaml
+oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f route.yaml
 oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f crb.yaml
 oc apply --server-side --field-manager=devopscenter.criticalmanufacturing --force-conflicts -f pvc.yaml
--- mesinabox/stack/removeNodeJSStack.ps1
+++ mesinabox/stack/removeNodeJSStack.ps1
@@ -1,6 +1,6 @@
 oc delete -f service.yaml
 oc delete -f deployment.yaml
-oc delete -f ingress.yaml
+oc delete -f route.yaml
 oc delete -f crb.yaml
 oc delete -f pvc.yaml
 
